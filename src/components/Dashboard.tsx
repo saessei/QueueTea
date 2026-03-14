4dba3e6ae848ea77ea4602d9d50467ede86463c4
saessei <danielleanne.poral-22@cpu.edu.ph>
2026-03-14 15:49:12 +0800

    feat: add signout button in dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import {UserAuth} from "../context/AuthContext";
+import  { useNavigate } from "react-router-dom";
+
 
 export const Dashboard = () => {
+  const { session, signOut} = UserAuth();
+  const navigate= useNavigate();
+
+  
+
+  const handleSignOut = async (e: any) => {
+    e.preventDefault()
+    try {
+      await signOut
+      navigate('/')
+    } catch(err) {
+      console.error(err);
+    }
+  }
+
   return (
-    <div>Dashboard</div>
+    <div>
+      <h1>Dashboard</h1>
+      <h2> Welcome, {session?.user?.email}</h2>
+        <div>
+        <p onClick={handleSignOut} className="hover:cursor-pointer border inline-block px-4 py-3 mt-4">Sign out</p>
+        </div>  
+    </div>
   )
 }
